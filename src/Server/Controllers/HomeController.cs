6a4d8d8bd313fc9bdd10b42565011413d0d842de
linianhui <linianhuidev@outlook.com>
2017-04-21 23:47:18 +0800

    init server project.

---
+++ src/Server/Controllers/HomeController.cs
@@ -0,0 +1,13 @@
+﻿using System.Web.Mvc;
+
+namespace Server.Controllers
+{
+    public class HomeController : Controller
+    {
+        [HttpGet]
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
---
+++ src/Server/Global.asax.cs
@@ -0,0 +1,13 @@
+﻿using System.Web;
+using System.Web.Routing;
+
+namespace Server
+{
+    public class MvcApplication : HttpApplication
+    {
+        protected void Application_Start()
+        {
+            RouteConfig.RegisterRoutes(RouteTable.Routes);
+        }
+    }
+}
