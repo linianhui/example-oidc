b3ecddb0b332b4e77663d083a0e28d59c9380a75
linianhui <linianhuidev@outlook.com>
2017-04-21 23:59:32 +0800

    add ids3 to server project.

---
+++ src/Server/Ids3/Use/Ids3Extentions.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections.Generic;
+using IdentityServer3.Core.Configuration;
+using IdentityServer3.Core.Models;
+using IdentityServer3.Core.Services.InMemory;
+using Owin;
+
+namespace Server.Ids3.Use
+{
+    public static class Ids3Extentions
+    {
+        public static void UseIds3(this IAppBuilder app)
+        {
+            app.Map("/identity", idsApp =>
+            {
+                idsApp.UseIdentityServer(new IdentityServerOptions
+                {
+                    SiteName = "Ids3 Server Web Site",
+                    IssuerUri = "http://demo.server.ids3",
+                    RequireSsl = false,
+                    SigningCertificate = Certificates.SigningCertificate,
+                    Factory = BuildIdentityServerServiceFactory(),
+                    LoggingOptions = new LoggingOptions
+                    {
+                        EnableWebApiDiagnostics = true
+                    }
+                });
+            });
+        }
+
+        private static IdentityServerServiceFactory BuildIdentityServerServiceFactory()
+        {
+            return new IdentityServerServiceFactory()
+                .UseInMemoryClients(new Client[0])
+                .UseInMemoryUsers(new List<InMemoryUser>())
+                .UseInMemoryScopes(StandardScopes.All);
+        }
+    }
+}
---
+++ src/Server/Startup.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.Owin;
+using Owin;
+using Server.Ids3.Use;
+
+[assembly: OwinStartup(typeof(Server.Startup))]
+
+namespace Server
+{
+    public class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            app.UseIds3();
+        }
+    }
+}
