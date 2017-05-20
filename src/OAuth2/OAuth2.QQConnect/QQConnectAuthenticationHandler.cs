91fe4e7a70522734856d661dcf610508497a428b
lnh <lnhcode@outlook.com>
2017-05-20 19:22:53 +0800

    qq.connect - Step.5 Use QQ UserInfo SignIn.

--- src/OAuth2/OAuth2.QQConnect/QQConnectAuthenticationHandler.cs
+++ src/OAuth2/OAuth2.QQConnect/QQConnectAuthenticationHandler.cs
@@ -5,11 +5,15 @@ using Newtonsoft.Json.Linq;
 using System;
 using System.Collections.Generic;
 using System.Net.Http;
+using System.Security.Claims;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace OAuth2.QQConnect
 {
+    /// <summary>
+    /// <see cref="http://wiki.connect.qq.com/%e5%bc%80%e5%8f%91%e6%94%bb%e7%95%a5_server-side"/>
+    /// </summary>
     internal class QQConnectAuthenticationHandler : AuthenticationHandler<QQConnectAuthenticationOptions>
     {
         private readonly ILogger _logger;
@@ -26,39 +30,55 @@ namespace OAuth2.QQConnect
             if (Options.CallbackPath == Request.Path.Value)
             {
                 var ticket = await AuthenticateAsync();
-                if (ticket == null)
+                if (ticket?.Identity != null)
+                {
+                    var identity = ticket.Identity;
+                    if (identity.AuthenticationType != Options.SignInAsAuthenticationType)
+                    {
+                        identity = new ClaimsIdentity(
+                            ticket.Identity.Claims,
+                            Options.SignInAsAuthenticationType,
+                            ticket.Identity.NameClaimType,
+                            ticket.Identity.RoleClaimType);
+                    }
+
+                    Context.Authentication.SignIn(ticket.Properties, identity);
+
+                    Context.Response.Redirect(ticket.Properties.RedirectUri);
+                }
+                else
                 {
                     _logger.WriteWarning("Invalid return state, unable to redirect.");
                     Response.StatusCode = 500;
-                    return true;
                 }
+                return true;
             }
             return false;
         }
 
         protected override async Task<AuthenticationTicket> AuthenticateCoreAsync()
         {
-            AuthenticationProperties properties = null;
+            AuthenticationProperties authenticationProperties = null;
 
             try
             {
                 var code = Request.Query.Get("code");
                 var state = Request.Query.Get("state");
 
-                properties = Options.StateDataFormat.Unprotect(state);
-                if (properties == null)
+                authenticationProperties = Options.StateDataFormat.Unprotect(state);
+                if (authenticationProperties == null)
                 {
                     return null;
                 }
 
-                if (!ValidateCorrelationId(properties, _logger))
+                if (!ValidateCorrelationId(authenticationProperties, _logger))
                 {
-                    return new AuthenticationTicket(null, properties);
+                    return new AuthenticationTicket(null, authenticationProperties);
                 }
 
                 if (code == null)
                 {
-                    return new AuthenticationTicket(null, properties);
+                    return new AuthenticationTicket(null, authenticationProperties);
                 }
 
                 var accessTokenResult = await GetAccessTokenResult(code);
@@ -66,25 +86,39 @@ namespace OAuth2.QQConnect
                 if (string.IsNullOrWhiteSpace(accessToken))
                 {
                     _logger.WriteError("access_token was not found");
-                    return new AuthenticationTicket(null, properties);
+                    return new AuthenticationTicket(null, authenticationProperties);
                 }
 
                 var openIdResult = await GetOpenIdResult(accessToken);
-                var openId = openIdResult.Value<string>("openid");
+                var openId = GetJsonValue(openIdResult, "openid");
                 if (string.IsNullOrWhiteSpace(openId))
                 {
                     _logger.WriteError("openid was not found");
-                    return new AuthenticationTicket(null, properties);
+                    return new AuthenticationTicket(null, authenticationProperties);
                 }
 
                 var userInfoResult = await GetUserInfoResult(accessToken, openId);
-                return null;
+
+                var identity = new ClaimsIdentity(Options.AuthenticationType, ClaimsIdentity.DefaultNameClaimType, ClaimsIdentity.DefaultRoleClaimType);
+
+                identity.AddClaim(new Claim(ClaimTypes.NameIdentifier, openId));
+                identity.AddClaim(new Claim("nickname", GetJsonValue(userInfoResult, "nickname")));
+                identity.AddClaim(new Claim("avatar", GetJsonValue(userInfoResult, "figureurl_qq_1")));
+
+                return new AuthenticationTicket(identity, authenticationProperties);
             }
             catch (Exception ex)
             {
                 _logger.WriteError("Authentication failed", ex);
-                return new AuthenticationTicket(null, properties);
+                return new AuthenticationTicket(null, authenticationProperties);
             }
+        }
+
+        private static string GetJsonValue(JObject json, string name)
+        {
+            JToken value;
+            json.TryGetValue(name, out value);
+            return value?.ToString() ?? string.Empty;
         }
 
         protected override Task ApplyResponseChallengeAsync()
@@ -118,6 +152,7 @@ namespace OAuth2.QQConnect
         {
             var response = await _httpClient.GetAsync(BuilUserInfoUrl(accessToken, openId), Request.CallCancelled);
             response.EnsureSuccessStatusCode();
+            //狗日的腾讯！！！json
             var json = await response.Content.ReadAsStringAsync();
             return JObject.Parse(json);
         }
@@ -126,8 +161,10 @@ namespace OAuth2.QQConnect
         {
             var response = await _httpClient.GetAsync(BuildOpenIdUrl(accessToken), Request.CallCancelled);
             response.EnsureSuccessStatusCode();
-            var text = await response.Content.ReadAsStringAsync();
-            var json = text.Substring(8).Trim().Trim('(', ')', ';');
+            //狗日的腾讯！！！jsonp
+            //callback( {"client_id":"YOUR_APPID","openid":"YOUR_OPENID"} );
+            var jsonp = await response.Content.ReadAsStringAsync();
+            var json = jsonp.Substring(8).Trim().Trim('(', ')', ';');
             return JObject.Parse(json);
         }
 
@@ -135,6 +172,8 @@ namespace OAuth2.QQConnect
         {
             var response = await _httpClient.GetAsync(BuildAccessTokenUrl(code), Request.CallCancelled);
             response.EnsureSuccessStatusCode();
+            //狗日的腾讯！！！Content-Type是text/html,格式却是form-urlencoded
+            //access_token=YOUR_ACCESS_TOKEN&expires_in=3600
             var text = await response.Content.ReadAsStringAsync();
             var keyValues = new Dictionary<string, string>();
             foreach (var param in text.Split('&'))
