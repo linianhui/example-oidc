15b438361b27fe499c29412860234af58f3f0ee6
lnh <lnhcode@outlook.com>
2017-08-05 18:18:45 +0800

    oauth2-resources-site add web api with owin.

---
+++ src/oauth2.resources-site/Apis/ValuesApiController.cs
@@ -0,0 +1,15 @@
+﻿using System.Web.Http;
+
+namespace OAuth2.Resources.Apis
+{
+    [RoutePrefix("values")]
+    public class ValuesApiController : ApiController
+    {
+        [HttpGet]
+        [Route("", Name = "get-values")]
+        public string[] GetValues()
+        {
+            return new[] { "value1", "value2", "lnhcode@outlook.com" };
+        }
+    }
+}
---
+++ src/oauth2.resources-site/Startup.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.Owin;
+using Owin;
+using System.Web.Http;
+
+[assembly: OwinStartup(typeof(OAuth2.Resources.Startup))]
+
+namespace OAuth2.Resources
+{
+    public class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            app.UseWebApi(GetWebApiConfig());
+        }
+
+        public static HttpConfiguration GetWebApiConfig()
+        {
+            var config = new HttpConfiguration();
+
+            config.Formatters.Remove(config.Formatters.XmlFormatter);
+
+            config.MapHttpAttributeRoutes();
+
+            config.Routes.MapHttpRoute(
+                name: "DefaultApi",
+                routeTemplate: "{controller}",
+                defaults: new { id = RouteParameter.Optional }
+            );
+
+            return config;
+        }
+    }
+}
