0fe598f1681ca33544fdda7c7ebce716d9072d00
lnh <lnhcode@outlook.com>
2018-07-01 19:21:17 +0800

    add docker-run.sh

---
+++ docker-run.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+dotnet publish ./src/web.oidc.server.ids4/web.oidc.server.ids4.csproj --output ../../www/oidc-server.test
+
+dotnet publish ./src/web.oidc.client.hybrid/web.oidc.client.hybrid.csproj --output ../../www/oidc-client-hybrid.test
+
+dotnet publish ./src/web.oauth2.resources.aspnetcore/web.oauth2.resources.aspnetcore.csproj --output ../../www/oauth2-resources-aspnetcore.test
+
+dotnet publish ./src/web.oauth2.client.aspnetcore/web.oauth2.client.aspnetcore.csproj --output ../../www/oauth2-client-aspnetcore.test
+
+cp -f -r ./src/web.oidc.client.js ./www/oidc-client-js.test
+
+docker-compose up -d
