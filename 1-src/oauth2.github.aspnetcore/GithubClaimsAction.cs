795693df56c1dd3af02b9b49709626a2c814be32
lnh <lnhcode@outlook.com>
2018-09-01 15:28:39 +0800

    Add OAuth2.Github.AspNetCore middleware.

---
+++ 1-src/oauth2.github.aspnetcore/GithubClaimsAction.cs
@@ -0,0 +1,28 @@
+﻿using System.Security.Claims;
+using Microsoft.AspNetCore.Authentication.OAuth.Claims;
+using Newtonsoft.Json.Linq;
+
+namespace OAuth2.Github.AspNetCore
+{
+    public class GithubClaimsAction : ClaimAction
+    {
+        public GithubClaimsAction()
+            : base("github", ClaimValueTypes.String)
+        {
+        }
+
+        public override void Run(JObject userData, ClaimsIdentity identity, string issuer)
+        {
+            if (userData != null)
+            {
+                foreach (var userDatum in userData)
+                {
+                    var key = userDatum.Key;
+                    var value = userDatum.Value.ToString();
+                    identity.AddClaim(new Claim("github." + key, value, ClaimValueTypes.String, issuer));
+                }
+                identity.AddClaim(new Claim(ClaimTypes.NameIdentifier, userData.GetValue("id").ToString(), ClaimValueTypes.String, issuer));
+            }
+        }
+    }
+}
