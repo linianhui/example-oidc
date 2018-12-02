a9fb073457ec8eaed7f1d1762720371b122239c6
lnh <lnhcode@outlook.com>
2018-12-02 10:00:26 +0800

    Add debug api

---
+++ 1-src/web.oidc.server.ids4/Controllers/DebugController.cs
@@ -0,0 +1,49 @@
+﻿using IdentityServer4.Models;
+using IdentityServer4.Test;
+using Microsoft.AspNetCore.Mvc;
+using ServerSite.Ids4;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace ServerSite.Controllers
+{
+    [Route(".debug")]
+    [ApiController]
+    public class DebugController : Controller
+    {
+
+        [HttpGet("clients", Name = "debug.clients.get")]
+        public IEnumerable<Client> GetClients()
+        {
+            return Clients.All;
+        }
+
+        [HttpGet("users", Name = "debug.users.get")]
+        public IEnumerable<TestUser> GetUsers()
+        {
+            return Users.All;
+        }
+
+        [HttpGet("user-claims", Name = "debug.user_claims.get")]
+        public JsonResult GetUser()
+        {
+            return Json(base.User?.Claims.Select(_ => new
+            {
+                type = _.Type,
+                value = _.Value
+            }));
+        }
+
+        [HttpGet("api-resources", Name = "debug.api_resources.get")]
+        public IEnumerable<ApiResource> GetApiResources()
+        {
+            return Ids4.Resources.AllApiResources;
+        }
+
+        [HttpGet("identity-resources", Name = "debug.identity_resources.get")]
+        public IEnumerable<IdentityResource> GetIdentityResources()
+        {
+            return Ids4.Resources.AllIdentityResources;
+        }
+    }
+}
--- 1-src/web.oidc.server.ids4/Controllers/HomeController.cs
+++ 1-src/web.oidc.server.ids4/Controllers/HomeController.cs
@@ -1,4 +1,7 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using Microsoft.AspNetCore.Http.Extensions;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
 
 namespace ServerSite.Controllers
 {
@@ -7,7 +10,21 @@ namespace ServerSite.Controllers
         [HttpGet]
         public ActionResult Index()
         {
-            return Content("<a href='/.well-known/openid-configuration'>ids4 discovery api</a>", "text/html");
+            string authority = new Uri(this.Request.GetDisplayUrl())
+                .GetLeftPart(UriPartial.Authority);
+
+            return Json(new Dictionary<string, object>
+            {
+                ["oidc_discovery_url"] = authority + "/.well-known/openid-configuration",
+                ["debug"] = new Dictionary<string, string>
+                {
+                    ["clients"] = this.Url.Link("debug.clients.get", null),
+                    ["users"] = this.Url.Link("debug.users.get", null),
+                    ["user_claims"] = this.Url.Link("debug.user_claims.get", null),
+                    ["api_resources"] = this.Url.Link("debug.api_resources.get", null),
+                    ["identity_resources"] = this.Url.Link("debug.identity_resources.get", null)
+                }
+            });
         }
     }
 }
