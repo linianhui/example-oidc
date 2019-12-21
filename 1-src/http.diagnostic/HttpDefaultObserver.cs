6819b4b69715561ecf258fd6fb1f5a2bc3726221
lnh <lnhcode@outlook.com>
2019-12-21 19:45:53 +0800

    Add http.diagnostic project

---
+++ 1-src/http.diagnostic/HttpDefaultObserver.cs
@@ -0,0 +1,88 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Net.Http;
+using Newtonsoft.Json.Linq;
+
+namespace Http.Diagnostic
+{
+    public class HttpDefaultObserver : IObserver<KeyValuePair<String, Object>>
+    {
+        public void OnCompleted()
+        {
+
+        }
+
+        public void OnError(Exception error)
+        {
+        }
+
+        public void OnNext(KeyValuePair<string, object> value)
+        {
+            var key = value.Key;
+            if (key == "System.Net.Http.Response")
+            {
+                var v = value.Value;
+                var response = v.GetType().GetProperty("Response").GetValue(v);
+                LogHttp(response as HttpResponseMessage);
+            }
+        }
+
+        private void LogHttp(HttpResponseMessage httpResponseMessage)
+        {
+            var color = ConsoleColor.Green;
+            if (httpResponseMessage.IsSuccessStatusCode == false)
+            {
+                color = ConsoleColor.Red;
+            }
+
+            LogLine(Environment.NewLine, color);
+            LogLine(new String('<', 32), color);
+            LogLine(Environment.NewLine, color);
+            LogHttpRequest(httpResponseMessage.RequestMessage, color);
+            LogLine(Environment.NewLine, color);
+            LogHttpResponse(httpResponseMessage, color);
+            LogLine(Environment.NewLine, color);
+            LogLine(new String('>', 32), color);
+            LogLine(Environment.NewLine, color);
+        }
+
+        private void LogHttpRequest(HttpRequestMessage request, ConsoleColor color)
+        {
+            LogLine($"{request.Method.Method} {request.RequestUri}", color);
+            LogLine(request.Headers, color);
+            LogHttpContent(request.Content, color);
+        }
+
+        private void LogHttpResponse(HttpResponseMessage response, ConsoleColor color)
+        {
+            LogLine($"{(Int32)response.StatusCode} {response.ReasonPhrase}", color);
+            LogLine(response.Headers, color);
+            LogHttpContent(response.Content, color);
+        }
+
+        private void LogHttpContent(HttpContent httpContent, ConsoleColor color)
+        {
+            if (httpContent == null)
+            {
+                return;
+            }
+            var responseContent = httpContent.ReadAsStringAsync().GetAwaiter().GetResult();
+            if (httpContent.Headers.ContentType.MediaType.Contains("json"))
+            {
+                LogLine(JObject.Parse(responseContent), color);
+            }
+            else
+            {
+                LogLine(responseContent, color);
+            }
+        }
+
+        private void LogLine(Object value, ConsoleColor color)
+        {
+            var oldColor = Console.ForegroundColor;
+            Console.ForegroundColor = color;
+            Console.WriteLine(value);
+            Console.ForegroundColor = oldColor;
+        }
+    }
+}
---
+++ 1-src/http.diagnostic/HttpDiagnosticListenerObserver.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Diagnostics;
+
+namespace Http.Diagnostic
+{
+    public class HttpDiagnosticListenerObserver : IObserver<DiagnosticListener>
+    {
+        public void OnCompleted()
+        {
+
+        }
+
+        public void OnError(Exception error)
+        {
+
+        }
+
+        public void OnNext(DiagnosticListener value)
+        {
+            value.Subscribe(new HttpDefaultObserver());
+        }
+    }
+}
