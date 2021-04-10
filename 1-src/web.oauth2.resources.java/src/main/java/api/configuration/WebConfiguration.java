9ee0f2f39b810a722f58604deb7dbd607d5dd691
lnh <lnhdyx@outlook.com>
2021-04-10 14:13:56 +0800

    fix error
    When allowCredentials is true, allowedOrigins cannot contain the special value "*“since that cannot be set on the “Access-Control-Allow-Origin” response header. To allow credentials to a set of origins, list them explicitly or consider using"allowedOriginPatterns” instead.

--- 1-src/web.oauth2.resources.java/src/main/java/api/configuration/WebConfiguration.java
+++ 1-src/web.oauth2.resources.java/src/main/java/api/configuration/WebConfiguration.java
@@ -10,7 +10,7 @@ public class WebConfiguration implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry cors) {
         cors.addMapping("/**")
-            .allowedOrigins("*")
+            .allowedOriginPatterns("*")
             .allowedMethods("*")
             .allowedHeaders("*")
             .allowCredentials(true);
